72de6746324243836d647d6b3a1b0bd9fa1f7c70
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-11-08 17:05:13 +0300

    Create whale_talk.cpp

---
+++ Codecademy Learn C++/5- Vectors/whale_talk.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::string input = "turpentine and turtles";
+    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    std::vector<char> result;
+
+    for (int i = 0; i < input.size(); i++)
+    {
+
+        for (int j = 0; j < vowels.size(); j++)
+        {
+
+            if (input[i] == vowels[j])
+            {
+
+                result.push_back(input[i]);
+            }
+        }
+
+        if (input[i] == 'u' || input[i] == 'e')
+        {
+
+            result.push_back(input[i]);
+        }
+    }
+    for (int k = 0; k < result.size(); k++)
+    {
+        std::cout << result[k];
+    }
+}
