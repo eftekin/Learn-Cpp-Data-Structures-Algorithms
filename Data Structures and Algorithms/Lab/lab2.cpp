8cd9c1d3922e24206e3222d132159ef906c65361
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-11-09 20:02:43 +0300

    update deleting function and switch(choice) case 2

--- Data Structures and Algorithms/Lab/lab2.cpp
+++ Data Structures and Algorithms/Lab/lab2.cpp
@@ -2,8 +2,7 @@
 #include <cassert>
 using namespace std;
 
-template<class hbi>
-
+template <class hbi>
 class linkedLists
 {
 private:
@@ -16,33 +15,59 @@ private:
 	Node *head, *tail;
 
 public:
-	
-linkedLists() 	{
-	head, tail=NULL;
-	length=0;
-}
+	linkedLists()
+	{
+		head, tail = NULL;
+		length = 0;
+	}
 
- 	bool isEmpty()
+	bool isEmpty()
 	{
 		return (length == 0);
 	}
 
-	void Deleting()
+	void Deleting(hbi item)
 	{
 		if (isEmpty())
 			cout << "Empty List" << endl;
-		else if (length == 1)
-		{
-			delete head;
-			tail = NULL;
-			length = 0;
-		}
 		else
 		{
 			Node *current = head;
-			head = head->next;
-			delete current;//free(current)
-			length--;
+			Node *prev = NULL;
+			bool found = false;
+
+			while (current != NULL)
+			{
+				if (current->item == item)
+				{
+					found = true;
+					break;
+				}
+
+				prev = current;
+				current = current->next;
+			}
+
+			if (found)
+			{
+				if (prev == NULL)
+				{
+					head = head->next;
+					delete current;
+				}
+				else
+				{
+					prev->next = current->next;
+					delete current;
+				}
+
+				length--;
+				cout << "Element " << item << " deleted successfully." << endl;
+			}
+			else
+			{
+				cout << "Element " << item << " not found in the list." << endl;
+			}
 		}
 	}
 
@@ -64,18 +89,19 @@ linkedLists() 	{
 
 	void display()
 	{
-		Node*cur = head;
+		Node *cur = head;
 		cout << "Current list is: [ ";
-		while (cur!=NULL)
+		while (cur != NULL)
 		{
-			cout << cur->item<<" ";
+			cout << cur->item << " ";
 			cur = cur->next;
-		}cout << "]" << endl;
+		}
+		cout << "]" << endl;
 	}
 
 	void search(hbi item)
 	{
-		Node*cur = head;
+		Node *cur = head;
 		bool flag = true;
 		while (cur != NULL)
 		{
@@ -87,68 +113,69 @@ linkedLists() 	{
 			}
 			cur = cur->next;
 		}
-		if(flag)
+		if (flag)
 			cout << "The element you entered: " << item << " not found in the list." << endl;
-
 	}
-
 };
 
 int main()
 {
-    int choice;
-    int z;
+	int choice;
+	int z;
 
-    cout << "=====================================================" << endl;
-    cout << "================ By Mustafa Eftekin =================" << endl;
-    cout << " " << endl;
-    cout << "       ~~ WELCOME to Linked List program ~~ " << endl;
-    cout << " " << endl;
+	cout << "=====================================================" << endl;
+	cout << "================ By Mustafa Eftekin =================" << endl;
+	cout << " " << endl;
+	cout << "       ~~ WELCOME to Linked List program ~~ " << endl;
+	cout << " " << endl;
 
-    linkedLists<int> linkedList;
-    for (int i = 1; i <= 25; i++)
-        linkedList.insert(i);
+	linkedLists<int> linkedList;
+	for (int i = 1; i <= 25; i++)
+		linkedList.insert(i);
 
-    while (true) {
-        cout << "Select and Enter Your Choice from the Menu :" << endl;
-        cout << "1. Add" << endl;
-        cout << "2. Delete" << endl;
-        cout << "3. Search" << endl;
-        cout << "4. Print" << endl;
-        cout << "5. Exit" << endl;
-        cout << "=====================================================" << endl;
-        cin >> choice;
+	while (true)
+	{
+		cout << "Select and Enter Your Choice from the Menu :" << endl;
+		cout << "1. Add" << endl;
+		cout << "2. Delete" << endl;
+		cout << "3. Search" << endl;
+		cout << "4. Print" << endl;
+		cout << "5. Exit" << endl;
+		cout << "=====================================================" << endl;
+		cin >> choice;
 
-        switch (choice) {
-        case 1:
-            linkedList.display();
-            cout << "Enter the element you want to add to the list." << endl;
-            cin >> z;
-            linkedList.insert(z);
-            linkedList.display();
-            cout << "Successfully added." << endl;
-            break;
-        case 2:
-            cout << "The first element of the list is deleted." << endl;
-            linkedList.Deleting();
-            linkedList.display();
-            break;
-        case 3:
-            cout << "Enter the element you want to search for in the list." << endl;
-            cin >> z;
-            linkedList.search(z);
-            linkedList.display();
-            break;
-        case 4:
-            linkedList.display();
-            break;
-        case 5:
-            cout << "Exiting the program." << endl;
-            return 0; // Programdan çıkış yap
-        default:
-            cout << "Invalid choice." << endl;
-        }
-    }
+		switch (choice)
+		{
+		case 1:
+			linkedList.display();
+			cout << "Enter the element you want to add to the list." << endl;
+			cin >> z;
+			linkedList.insert(z);
+			linkedList.display();
+			cout << "Successfully added." << endl;
+			break;
+		case 2:
+			cout << "Enter the element you want to delete from the list." << endl;
+			cin >> z;
+			linkedList.Deleting(z);
+			linkedList.display();
+			break;
+		case 3:
+			cout << "Enter the element you want to search for in the list." << endl;
+			cin >> z;
+			linkedList.search(z);
+			linkedList.display();
+			break;
+		case 4:
+			linkedList.display();
+			break;
+		case 5:
+			cout << "Exiting the program." << endl;
+			return 0;
+		default:
+			cout << "Invalid choice." << endl;
+		}
+	}
 
-    return 0;
+	return 0;
 }
